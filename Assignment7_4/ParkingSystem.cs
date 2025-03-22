88d4d991fae3354a7106566962b6e66c46e42e2b
pangzhen82 <pangzhen82@gmail.com>
2025-03-21 21:51:06 -0600

    Add files via upload

---
+++ Assignment7_4/ParkingSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7_4
+{
+    internal class ParkingSystem
+    {
+        private int[] parkingSpaces;
+        //public int Availability { get; private set; }
+
+        public ParkingSystem(int big, int medium, int small)
+        {
+            parkingSpaces = new int[3];
+            parkingSpaces[0] += big;
+            parkingSpaces[1] += medium;
+            parkingSpaces[2] += small;
+            //Availability += big;
+            //Availability += medium;
+            //Availability += small;
+        }
+
+        public bool AddCar(int carType)
+        {
+            if (carType < 1 || carType > 3) return false;
+
+            if (parkingSpaces[carType - 1] > 0)
+            {
+                parkingSpaces[carType - 1]--;
+                //this.Availability--;
+                return true;
+            }
+            return false;
+        }
+    }
+
+
+}
---
+++ Assignment7_4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment7_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // --- Test parking system ---
+            TestParkingSystemProg();
+
+            Console.ReadKey();
+        } 
+
+        static void TestParkingSystemProg()
+        {
+            Console.WriteLine("\n--- Test parking system ---\n");
+            ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+            Console.WriteLine(parkingSystem.AddCar(1)); // return true because there is 1 available slot for a big car
+            Console.WriteLine(parkingSystem.AddCar(2)); // return true because there is 1 available slot for a medium car
+            Console.WriteLine(parkingSystem.AddCar(3)); // return false because there is no available slot for a small car
+            Console.WriteLine(parkingSystem.AddCar(1)); // return false because there is no available slot for a big car. It is already occupied.
+        }
+    }
+}
