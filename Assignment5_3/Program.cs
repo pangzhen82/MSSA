573c35bb9279607233641267911a2227cdf9c7d9
pangzhen82 <pangzhen82@gmail.com>
2025-03-23 22:48:49 -0600

    Add files via upload

---
+++ Assignment5_3/Program.cs
@@ -0,0 +1,128 @@
+﻿namespace Assignment5_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // --- Check to see if possible to plant N plants ---
+            CheckToPlantProg();
+            // --- Count ways to climb the stairs ---
+            NStepsProg();
+
+            Console.ReadKey();
+        }
+        static void CheckToPlantProg()
+        {
+            int[] array;
+            int plant;
+            char option = 'y';
+
+            Console.WriteLine("\n--- Check to see if possible to plant N plants ---\n");
+            while (option == 'y' || option == 'Y')
+            {
+                array = InitializeIntArray();
+                plant = GetInt();
+                PrintArray(array);
+                Console.WriteLine($"Possibility to plant {plant} in the flower bed is - {CheckToPlant(array, plant)}");
+                option = GetOption();
+            }
+        }
+        static bool CheckToPlant(int[] arr, int plant) // Time complexity O(N)
+        {
+            if (arr.Length == 0) return false;
+            int count = 1, possible = 0;    // count starts from 1 to give the possibility to plant from first place [0, 0, 1, ...]
+
+            foreach (int i in arr)
+            {
+                if (i == 0)
+                    count++;
+                else
+                {
+                    possible += ((count + 1) / 2 - 1); // 0 -> 0, 00 -> 0, 000 -> 1,  0000 -> 1, 
+                    count = 0;
+                }
+            }
+
+            possible += ((count + 2) / 2 - 1);  // count + 2 to give the possibility to plant at the last position [..., 1, 0, 0]
+            return possible >= plant;
+        }
+        static void NStepsProg()
+        {
+            Console.WriteLine("\n--- Count ways to climb the stairs ---\n");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"There are {NSteps(i)} ways to climb {i} stairs to the top.");
+            }
+        }
+        static int NSteps (int stairs) // Recursion 
+        {
+            if (stairs <= 2) return stairs;
+            else
+                return NSteps(stairs - 1) + NSteps(stairs - 2);      // 0->0, 1->1, 2->2, 3->3, 4->5, 5->8
+        }
+        static int[] InitializeIntArray() 
+        {
+            bool isValid;
+            int size;
+            int[] array;
+
+        getSize:
+            Console.Write("Enter the size of the array: ");
+            isValid = int.TryParse(Console.ReadLine(), out size);
+            if (!isValid || size == 0)
+            {
+                Console.WriteLine("\nError! Enter a valid integer!\n");
+                goto getSize;
+            }
+            array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+        getNumber:
+                Console.Write($"Element - {i + 1}: ");
+                isValid = int.TryParse(Console.ReadLine(), out array[i]);
+                if (!isValid || array[i] > 1)   // check if it is larger than 1 
+                {
+                    Console.WriteLine("\nError! Enter valid integer: (0 or 1)");
+                    goto getNumber;
+                }
+            }
+            return array;
+        }
+        static int GetInt()
+        {
+            bool isValid;
+            int num;
+        getNumber:
+            Console.Write($"Enter a number: ");
+            isValid = int.TryParse(Console.ReadLine(), out num);
+            if (!isValid || num < 1)
+            {
+                Console.WriteLine("\nError! Enter valid integer!");
+                goto getNumber;
+            }
+            return num;
+        }
+        static char GetOption()
+        {
+            bool isValid;
+            char c;
+            getChar:
+            Console.Write("\nContinue? (Y/N) ");
+            isValid = char.TryParse(Console.ReadLine().Trim(), out c);
+            if(!isValid && (c != 'y' || c != 'Y' || c != 'n' || c != 'N'))
+            {
+                Console.WriteLine("\nError! Enter a valid option!");
+                goto getChar;
+            }
+            return c;
+        }
+        static void PrintArray(int[] arr)
+        {
+            Console.Write("\n[");
+            foreach (int i in arr)
+                Console.Write(i + ", ");
+            Console.WriteLine("\b\b]");
+        }
+    }
+}
---
+++ Assignment5_4/Program.cs
@@ -0,0 +1,138 @@
+﻿namespace Assignment5_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // -- Use recursion function to print digits of number --
+            PrintdigitProg();
+
+            // -- Get the sum of right diagonal of matrix --
+            SumOfRightDiagonalsProg();
+
+            Console.ReadKey();
+        }
+
+
+        static void PrintdigitProg()
+        {
+            Console.WriteLine("\n-- Use recursion function to print digits of number --\n");
+            int num = GetInt();
+            Console.Write($"The digits in the number {num} are: ");
+            PrintDigit_Recursion(num);
+            Console.WriteLine();
+        }
+        static void SumOfRightDiagonalsProg()
+        {
+            Console.WriteLine("\n-- Get the sum of right diagonal of matrix --\n");
+            int [,] matrix = InitializeTwoDArray();
+            Print2DArray(matrix);
+            Console.WriteLine($"\nAddition of the right Diagonal elements is : {SumOfRightDiagonals(matrix)}");
+        }
+        static int SumOfRightDiagonals(int[,] matrix)
+        {
+            if (matrix == null) return -1;
+            int sum = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i == j)
+                        sum += matrix[i, j];
+                }
+            }
+            return sum;
+        }
+        static void PrintDigit_Recursion(int num)
+        {
+            if (num < 10)
+            {
+                Console.Write(num + " ");
+                return;
+            }
+            else
+            {
+                PrintDigit_Recursion(num / 10);
+                Console.Write(num % 10 + " ");
+            }
+        }
+        static int GetInt()
+        {
+            bool isValid;
+            int num;
+        getNumber:
+            Console.Write($"Enter a number: ");
+            isValid = int.TryParse(Console.ReadLine(), out num);
+            if (!isValid || num < 1)
+            {
+                Console.WriteLine("\nError! Enter valid integer!\n");
+                goto getNumber;
+            }
+            return num;
+        }
+        static int[,] InitializeTwoDArray()
+        {
+            int row, col;
+            int[,] matrix;
+
+            GetUserRowAndCol(out row, out col);
+            matrix = new int[row, col];
+            InputMatrix(matrix);
+            return matrix;
+        }
+        static void InputMatrix(int[,] matrix)
+        {
+            bool isValid;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                getVal:
+                    Console.Write($"Enter number in matrix[{i}][{j}]: ");
+                    isValid = int.TryParse(Console.ReadLine(), out matrix[i, j]);
+                    if (!isValid)
+                    {
+                        Console.WriteLine("\nError! Enter a valid integer!\n");
+                        goto getVal;
+                    }
+                }
+            }
+        }
+        static void GetUserRowAndCol(out int row, out int col)
+        {
+            bool isValid;
+        getRow:
+            Console.Write("Enter number of rows: ");
+            isValid = int.TryParse(Console.ReadLine(), out row);
+            if (!isValid || row == 0)
+            {
+                Console.WriteLine("\nError! Enter a valid integer!\n");
+                goto getRow;
+            }
+
+        getCol:
+            Console.Write("Enter number of columes: ");
+            isValid = int.TryParse(Console.ReadLine(), out col);
+            if (!isValid || col == 0)
+            {
+                Console.WriteLine("\nError! Enter a valid integer!\n");
+                goto getCol;
+            }
+        }
+        static void Print2DArray(int[,] matrix)
+        {
+            if (matrix == null) return;
+            Console.WriteLine("\nMatrix:");
+            for (int i = 0;i < matrix.GetLength(0);i++)
+            {
+                Console.Write("[");
+                for (int j = 0;j < matrix.GetLength(1);j++)
+                {
+                    Console.Write(matrix[i,j] + ", ");
+                }
+                Console.WriteLine("\b\b]");
+            }
+        }
+    }
+}
