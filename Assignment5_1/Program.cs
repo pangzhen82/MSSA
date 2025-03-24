78858c32da39df074a2a13987fab270a24fd63f8
pangzhen82 <pangzhen82@gmail.com>
2025-03-23 22:49:35 -0600

    Add files via upload

---
+++ Assignment5_1/Program.cs
@@ -0,0 +1,134 @@
+﻿namespace Assignment5_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // -- Check if an integer is a palindrome
+            IsIntegerPalindromeProg();
+
+            // -- Get sum of all digits in an integer --
+            //GetSumofDigitsProg();
+
+            // -- Check if array contains duplicate elements --
+            //CheckDuplicateElementsProg();
+
+            Console.ReadKey();
+        }
+
+
+        static void CheckDuplicateElementsProg()
+        {
+            Console.WriteLine("\n -- Check if array contains duplicate elements --\n");
+            int[] arr = InitializeIntArray();
+            Console.WriteLine($"Array contains duplicate elements: {ContainsDuplicateElements(arr)}");
+        }
+        static bool ContainsUniqueElements(int[] arr)
+        {
+            HashSet<int> set = new HashSet<int>();
+            foreach (int i in arr)
+            {
+                if (set.Contains(i)) return false;
+                else set.Add(i);
+            }
+            return true;
+        }
+        static bool ContainsDuplicateElements(int[] arr)
+        {
+            HashSet<int> set = new HashSet<int>();
+            foreach (int i in arr)
+            {
+                if (set.Contains(i)) return true;
+                else set.Add(i);
+            }
+            return false;
+        }
+        static void GetSumofDigitsProg()
+        {
+            int number, sum;
+            bool isValid;
+
+            Console.WriteLine("\n -- Get the sum of all digits in an integer --\n");
+        getNumber:
+            Console.Write("Enter a number: ");
+
+            isValid = int.TryParse(Console.ReadLine(), out number);
+            if (!isValid)
+            {
+                Console.WriteLine("\nError! Enter a valid integer!\n");
+                goto getNumber;
+            }
+            sum = GetSumofDigits(number);
+            Console.WriteLine($"The sum of the digits of the number {number} is : {sum}");
+        }
+        static int GetSumofDigits(int num)
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum;
+        }
+        static void IsIntegerPalindromeProg()
+        {
+            int number;
+            bool isValid;
+
+            Console.WriteLine(" -- Check if an integer is a palindrome number --\n");
+        getNumber:
+            Console.Write("Enter a number: ");
+
+            isValid = int.TryParse(Console.ReadLine(), out number);
+            if (!isValid)
+            {
+                Console.WriteLine("\nError! Enter a valid integer!\n");
+                goto getNumber;
+            }
+            IsIntPalindrome(number);
+            Console.WriteLine($"Number: {number} is a palindrome - {IsIntPalindrome(number)}");
+        }
+        static bool IsIntPalindrome(int target)
+        {
+            int reverse = 0;
+            int num = target;
+            while (num > 0)
+            {
+                reverse = reverse * 10 + num % 10;
+                Console.WriteLine($"{reverse} = {reverse} * 10 + {num} % 10");
+                num /= 10;
+            }
+            return reverse == target;
+        }
+        static int[] InitializeIntArray()
+        {
+            bool isValid;
+            int size;
+            int[] array;
+
+        getSize:
+            Console.Write("Enter the size of the array: ");
+            isValid = int.TryParse(Console.ReadLine(), out size);
+            if (!isValid || size == 0)
+            {
+                Console.WriteLine("\nError! Enter a valid integer!\n");
+                goto getSize;
+            }
+            array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+            getNumber:
+                Console.Write($"Element - {i + 1}: ");
+                isValid = int.TryParse(Console.ReadLine(), out array[i]);
+                if (!isValid)
+                {
+                    Console.WriteLine("\nError! Enter valid integer: ");
+                    goto getNumber;
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ Assignment5_2/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Assignment5_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // --- Get the length of the last word in string ----
+            GetLengthOfLastWordProg();
+
+            // --- Print the first 1 to N and N to 1 Numbers ---
+            PrintFirstNatrualNumberProg();
+
+            //--- Check if a string is palindrome with recursion ----
+            CheckPalindromeProg();
+
+            Console.ReadKey();
+        }
+
+        static void CheckPalindromeProg()
+        {
+            Console.WriteLine("\n--- Check if a string is palindrome with recursion ----\n");
+            Console.WriteLine("Enter a string: ");
+            string str = Console.ReadLine();
+            Console.WriteLine($"String - \"{str}\" is a palindrome: {IsPalindrome_Recursion(str.Trim().ToLower(), 0, str.Trim().Length - 1)}");
+        }
+        static void GetLengthOfLastWordProg()
+        {
+            Console.WriteLine("\n--- Get the length of the last word in string ----\n");
+            Console.WriteLine("Enter a sentence: ");
+            string str = Console.ReadLine();
+            Console.WriteLine($"The length of the last word is {LengthOfLastWord(str)}");
+        }
+        static void PrintFirstNatrualNumberProg()
+        {
+            int num;
+            bool isValid;
+            Console.WriteLine("\n--- Print the first 1 to N and N to 1 Numbers ---\n");
+        getNumber:
+            Console.Write("Enter a number to print: ");
+            isValid = int.TryParse(Console.ReadLine(), out num);
+            if(!isValid || num < 1)
+            {
+                Console.WriteLine("\nError! Enter a valid integer!\n");
+                goto getNumber;
+            }
+            Console.WriteLine("\n---Printing first 1 to N numbers ---\n");
+            Print1ToN(num);
+            Console.WriteLine("\n---Printing first N to 1 numbers ---\n");
+            PrintNTo1(num);
+        }
+        static int LengthOfLastWord(string str)
+        {
+            if (str == string.Empty) return 0;
+            return str.Trim().Split(" ")[str.Trim().Split(" ").Length - 1].Length;
+        }
+        static void Print1ToN(int n)
+        {
+            if (n > 0)
+            {
+                Print1ToN(n - 1);
+                Console.WriteLine(n);
+            }
+        }
+        static void PrintNTo1(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine(n);
+                PrintNTo1(n - 1);
+            }
+        }
+        static bool IsPalindrome_Recursion(string str, int left, int right)
+        {
+            if (left >= right) 
+                return true;
+            else if(str[left] != str[right]) 
+                return false;
+            else 
+                return IsPalindrome_Recursion(str, left + 1, right - 1);
+        }
+    }
+}
