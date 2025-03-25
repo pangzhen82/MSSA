597a9c8e82813648c25c9d970e5eba6b64d78364
pangzhen82 <pangzhen82@gmail.com>
2025-03-24 22:46:04 -0600

    Add files via upload

---
+++ Assignment11_1Books_WPF_SQLite/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Assignment11_1Books_WPF_SQLite.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Assignment11_1Books_WPF_SQLite
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ServiceProvider _serviceProvider;
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+
+            services.AddDbContext<BookContext> ( option =>
+            {
+                option.UseSqlite("Data Source = Books_WPF_SQLite.db");
+            });
+
+            services.AddSingleton<MainWindow>();
+            _serviceProvider = services.BuildServiceProvider ();
+        }
+        private void OnStartUp(object sender, StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+
+}
---
+++ Assignment11_1Books_WPF_SQLite/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Assignment11_1Books_WPF_SQLite.Data;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment11_1Books_WPF_SQLite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BookContext _dbContext;
+        Book _newBook = new Book();
+        Book? _selectedBook = new Book();
+
+        public MainWindow(BookContext _dbContext)
+        {
+            this._dbContext = _dbContext;
+            InitializeComponent();
+            GetBooks();
+            AddBookGrid.DataContext = _newBook;
+        }
+        private void GetBooks()
+        {
+            BooksDataGrid.ItemsSource = this._dbContext.Books.ToList();
+        }
+        private void UpdateBookForEdit (object sender, RoutedEventArgs e)
+        {
+            _selectedBook = (sender as FrameworkElement).DataContext as Book;
+            UpdateBookGrid.DataContext = _selectedBook;
+        }
+        private void DeleteBook (object sender, RoutedEventArgs e)
+        {
+            var book = (sender as FrameworkElement).DataContext as Book;
+            this._dbContext.Books.Remove(book);
+            this._dbContext.SaveChanges();
+            GetBooks();
+        }
+        private void AddBook (object sender, RoutedEventArgs e)
+        {
+            this._dbContext.Books.Add(_newBook);
+            this._dbContext.SaveChanges();
+            GetBooks();
+            _newBook = new Book();
+            AddBookGrid.DataContext= _newBook;
+        }
+        private void UpdateBook (object sender, RoutedEventArgs e)
+        {
+            this._dbContext.Books.Update(_selectedBook);
+            this._dbContext.SaveChanges();
+            _selectedBook = null;
+            UpdateBookGrid.DataContext = _selectedBook;
+            GetBooks();
+        }
+    }
+}
