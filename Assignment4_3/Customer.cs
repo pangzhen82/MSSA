a3911aca3fa078e4e54680fad5391fa2b87f8f5b
pangzhen82 <pangzhen82@gmail.com>
2025-03-25 22:26:32 -0600

    Add files via upload

---
+++ Assignment4_3/Customer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_3
+{
+    internal class Customer
+    {
+        static private int ID = 1001;
+        public int cusID;
+        public string Name { get; set; }
+        public int Unit {  get; set; }
+
+        public double Surcharge { get { return this.surcharge; } }
+
+        private int CusID { get {return this.cusID; } }
+        private double electricBill;
+        private double unitCharge;
+        private double surcharge;
+
+        public double ElectricBill { get { return this.electricBill; } }
+        public double UnitCharge { get { return this.unitCharge; } }
+
+        public Customer(string name, int unit)
+        {
+            this.cusID = ID++;
+            this.Name = name;
+            this.Unit = unit;
+            CalculateUnitCharge();
+            CalculateElectricBill();
+        }
+
+        private void CalculateUnitCharge()
+        {
+            if (this.Unit >600)
+            {
+                this.unitCharge = 2.0d;
+            }
+            else if (this.Unit > 400)
+            {
+                this.unitCharge = 1.8d;
+            }
+            else if (this.Unit > 200)
+            {
+                this.unitCharge = 1.5d;
+            }
+            else if (this.Unit >= 0)
+            {
+                this.unitCharge = 1.2d;
+            }
+        }
+
+        private void ApplySurcharge()
+        {
+            if (this.electricBill > 400)
+            {
+                this.surcharge = this.ElectricBill * 0.15;
+                this.electricBill += this.surcharge;
+            }
+            else
+                this.surcharge = 0d;
+        }
+        private void CalculateElectricBill()
+        {
+            this.electricBill = this.unitCharge * this.Unit;
+            ApplySurcharge();
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("\n************************   Electric Bill   ******************************\n");
+            Console.WriteLine("-------------------------------------------------------------------------\n");
+            Console.WriteLine($"Customer ID No. : \t{this.CusID}");
+            Console.WriteLine($"Customer name: \t\t{this.Name}");
+            Console.WriteLine($"Unit consumed: \t\t{this.Unit}\n");
+            Console.WriteLine($"Amount charges @${this.unitCharge.ToString("0.00")} per unit : \t\t${(this.unitCharge * this.Unit).ToString("0.00")}");
+            Console.WriteLine($"Surcharge Amount: \t\t\t\t${this.Surcharge.ToString("0.00")}");
+            Console.WriteLine($"Net amount paid by the customer: \t\t${this.ElectricBill.ToString("0.00")}");
+            Console.WriteLine("-------------------------------------------------------------------------\n");
+        }
+    }
+}
---
+++ Assignment4_3/Program.cs
@@ -0,0 +1,149 @@
+﻿namespace Assignment4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // -- Calculate the electric bill --
+            CalculateElectricBillProg();
+
+            // -- Count frequency of each item in an array --
+            CountArrayElementFrequencyProg();
+
+            // -- Find unique elements in an array --
+            GetUniqueNumberProg();
+
+            Console.ReadKey();
+        }
+        static void CountArrayElementFrequencyProg()
+        {
+            Dictionary<int, int> countTable = new Dictionary<int, int>();
+
+            Console.WriteLine("\n -- Count the frequecy of each element in array --\n");
+
+            int[] array = InitializeIntArray();
+
+            foreach (int num in array)
+            {
+                if (countTable.ContainsKey(num))
+                    countTable[num]++;
+                else
+                    countTable.Add(num, 1);
+            }
+
+            foreach (var item in countTable)
+            {
+                Console.WriteLine($"{item.Key} occurs {item.Value} times.");
+            }
+        }
+        static void CalculateElectricBillProg()
+        {
+            int id, unit;
+            string name;
+            double bill;
+            bool isValid;
+            char option = 'y';
+            Console.WriteLine("\n -- Calculate electric bill --\n");
+
+            while (option == 'y' || option == 'Y')
+            {
+            getName:
+                Console.Write("Enter customer Name: ");
+                name = Console.ReadLine();
+
+            getUnit:
+                Console.Write("Enter unit customer used: ");
+                isValid = int.TryParse(Console.ReadLine(), out unit);
+                if (!isValid)
+                {
+                    Console.WriteLine("\nError! Enter valid integer!\n");
+                    goto getUnit;
+                }
+
+                Customer customer = new Customer(name, unit);
+                customer.Display();
+
+                Console.WriteLine("\nContinue? (y/n)");
+                getOption:
+                isValid = char.TryParse(Console.ReadLine(), out option);
+                if (!isValid)
+                {
+                    Console.WriteLine("Error! Enter a valid option!\n");
+                    goto getOption;
+                }
+            }
+        }
+        static void GetUniqueNumberProg()
+        {
+            Console.WriteLine("\n -- Find the unique elements in array --\n");
+            int[] array = InitializeIntArray();
+            List<int> result = GetUniqueNumbers(array);
+            Console.WriteLine("\nThe unique element(s) found in the array are: ");
+            foreach (var item in result)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        static int[] InitializeIntArray()
+        {
+            bool isValid;
+            int size;
+            int[] array;
+
+        getSize:
+            Console.Write("Enter the size of the array: ");
+            isValid = int.TryParse(Console.ReadLine(), out size);
+            if (!isValid && size == 0)
+            {
+                Console.WriteLine("\nError! Enter a valid integer!\n");
+                goto getSize;
+            }
+            array = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+        getNumber:
+                Console.Write($"Element - {i + 1}: ");
+                isValid = int.TryParse(Console.ReadLine(), out array[i]);
+                if (!isValid)
+                {
+                    Console.WriteLine("\nError! Enter valid integer: ");
+                    goto getNumber;
+                } 
+            }
+            return array;
+        }
+        static List<int> GetUniqueNumbers(int[] nums)
+        {
+            Dictionary<int, int> countTable = new Dictionary<int, int>();
+            List<int> result = new List<int>();
+
+            foreach (int num in nums)
+            {
+                if (countTable.ContainsKey(num))
+                {
+                    countTable[num]++;
+                }
+                else
+                {
+                    countTable.Add(num, 1);
+                }
+            }
+
+            if (countTable.Count > 0)
+            {
+                foreach(var current in countTable)
+                {
+                    if (current.Value == 1)
+                    {
+                        result.Add(current.Key);
+                    }
+                }
+                return result;
+            }
+            return null;
+            
+        }
+    }
+}
